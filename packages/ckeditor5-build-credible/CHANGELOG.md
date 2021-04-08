b903999aa7759998d23ec574c2be75c8a1c89523
Dave Sag <davesag@gmail.com>
2021-04-08 10:28:24 +1000

    ANZIC-725 added custom Credible ckeditor builds

---
+++ packages/ckeditor5-build-credible/CHANGELOG.md
@@ -0,0 +1,8 @@
+# Changelog
+
+- [`19.0.0-credible-develop`] Custom Build for Credible added (2021-05-08)
+- [`19.0.0`](https://github.com/ckeditor/ckeditor5-build-classic/compare/v18.0.0...v19.0.0) (2020-04-29)
+
+    Build on top of [`CKEditor 5 v19.0.0.`](https://github.com/ckeditor/ckeditor5/releases/tag/v19.0.0)
+
+    In general code will be kept in sync with the latest stable release.
---
+++ packages/ckeditor5-build-credible/src/ckeditor.js
@@ -0,0 +1,138 @@
+/**
+ * @license Copyright (c) 2003-2021, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or https://ckeditor.com/legal/ckeditor-oss-license
+ */
+
+// The editor creator to use.
+import ClassicEditor from '@ckeditor/ckeditor5-editor-classic/src/classiceditor';
+import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
+import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
+import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
+import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
+import CKFinderUploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
+import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
+import Heading from '@ckeditor/ckeditor5-heading/src/heading';
+import Image from '@ckeditor/ckeditor5-image/src/image';
+import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
+import ImageStyle from '@ckeditor/ckeditor5-image/src/imagestyle';
+import ImageToolbar from '@ckeditor/ckeditor5-image/src/imagetoolbar';
+import ImageUpload from '@ckeditor/ckeditor5-image/src/imageupload';
+import Indent from '@ckeditor/ckeditor5-indent/src/indent';
+import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
+import Link from '@ckeditor/ckeditor5-link/src/link';
+import List from '@ckeditor/ckeditor5-list/src/list';
+import MediaEmbed from '@ckeditor/ckeditor5-media-embed/src/mediaembed';
+import Paragraph from '@ckeditor/ckeditor5-paragraph/src/paragraph';
+import PasteFromOffice from '@ckeditor/ckeditor5-paste-from-office/src/pastefromoffice';
+import PresenceList from '@ckeditor/ckeditor5-real-time-collaboration/src/presencelist';
+import RealTimeCollaborativeComments from '@ckeditor/ckeditor5-real-time-collaboration/src/realtimecollaborativecomments';
+import RealTimeCollaborativeEditing from '@ckeditor/ckeditor5-real-time-collaboration/src/realtimecollaborativeediting';
+import RealTimeCollaborativeTrackChanges from '@ckeditor/ckeditor5-real-time-collaboration/src/realtimecollaborativetrackchanges';
+import Table from '@ckeditor/ckeditor5-table/src/table';
+import TableToolbar from '@ckeditor/ckeditor5-table/src/tabletoolbar';
+import TextTransformation from '@ckeditor/ckeditor5-typing/src/texttransformation';
+
+import CKContext from '@ckeditor/ckeditor5-core/src/context';
+
+const BASE_PLUGINS = [
+	Autoformat,
+	BlockQuote,
+	Bold,
+	CKFinder,
+	CKFinderUploadAdapter,
+	Essentials,
+	Heading,
+	Image,
+	ImageCaption,
+	ImageStyle,
+	ImageToolbar,
+	ImageUpload,
+	Indent,
+	Italic,
+	Link,
+	List,
+	MediaEmbed,
+	Paragraph,
+	PasteFromOffice,
+	Table,
+	TableToolbar,
+	TextTransformation
+];
+
+const INTERACTIVE_PLUGINS = [
+	...BASE_PLUGINS,
+	PresenceList,
+	RealTimeCollaborativeComments,
+	RealTimeCollaborativeEditing,
+	RealTimeCollaborativeTrackChanges
+];
+
+export const BASE_CONFIG = {
+	toolbar: {
+		items: [
+			'heading',
+			'|',
+			'bold',
+			'italic',
+			'link',
+			'bulletedList',
+			'numberedList',
+			'|',
+			'outdent',
+			'indent',
+			'|',
+			'uploadImage',
+			'blockQuote',
+			'insertTable',
+			'mediaEmbed',
+			'undo',
+			'redo'
+		]
+	},
+	image: {
+		toolbar: [
+			'imageStyle:full',
+			'imageStyle:side',
+			'|',
+			'imageTextAlternative'
+		]
+	},
+	table: {
+		contentToolbar: [
+			'tableColumn',
+			'tableRow',
+			'mergeTableCells'
+		]
+	},
+	// This value must be kept in sync with the language defined in webpack.config.
+	language: 'en'
+};
+
+export const INTERACTIVE_CONFIG = {
+	...BASE_CONFIG,
+	cloudServices: {
+		// supply these values in the context
+		// tokenUrl,
+		// uploadUrl,
+		// webSocketUrl
+	}
+}
+
+export default class CredibleEditor extends ClassicEditor {}
+
+// Plugins to include in the build.
+CredibleEditor.builtinPlugins = [...BASE_PLUGINS];
+
+// Editor configuration.
+CredibleEditor.defaultConfig = {...BASE_CONFIG};
+
+export class InteractiveCredibleEditor extends ClassicEditor {}
+
+// Plugins to include in the build.
+InteractiveCredibleEditor.builtinPlugins = [...INTERACTIVE_PLUGINS];
+
+// Editor configuration.
+InteractiveCredibleEditor.defaultConfig = {...INTERACTIVE_CONFIG};
+
+// and the Context object to be shared.
+export const Context = CKContext;
